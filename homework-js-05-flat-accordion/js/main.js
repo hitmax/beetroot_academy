602aa62fd3d130bf2970ac0b0381f15dce53c2ca
hitmax <hitmax.rider@gmail.com>
2018-12-02 12:27:36 +0200

    edit homework-js-05 -flat-accordion TODO: remove TABS from lesson
    add lesson-js-08-slick-slider

--- homework-js-05-flat-accordion/js/main.js
+++ homework-js-05-flat-accordion/js/main.js
@@ -1,12 +1,47 @@
 ;(function ($) {
     $(function () {
 
-        $('.head').on('click', function () {
+        //my variant, click on current open element dont hide it
+
+        /*$('.head').on('click', function () {
             if ($('p').hasClass('show')) {
                 $('p').removeClass('show');
             }
             $(this).siblings().addClass('show');
+        })*/
+
+        $('.head').on('click', function () {
+
+            var section = $(this).parent();
+
+            if (section.hasClass('show')) {
+                section.removeClass('show');
+            } else {
+
+                section
+                    .addClass('show')
+                    .siblings()
+                    .removeClass('show')
+            }
         })
+
+        //for tabs
+        var tabsContent = $('.tabs-content')
+
+        $('.tabs a').on('click', function () {
+            var $this = $(this),
+                tabIndex = $this.data('id');
+            $this
+                .addClass('active')
+                .siblings()
+                .removeClass('active');
+            tabsContent
+                .find('[data-id = ' + tabIndex + ']')
+                .addClass('active')
+                .siblings()
+                .removeClass('active');
+        })
+
 
     })
 })(jQuery);
---
+++ lesson-js-08-slick-slider/js/main.js
@@ -0,0 +1,11 @@
+;(function ($) {
+    $(function () {
+
+        $('.slick-slider').slick({
+            autoplay: true,
+            autoplaySpeed: 2000,
+            dots: true
+        });
+
+    });
+})(jQuery);
