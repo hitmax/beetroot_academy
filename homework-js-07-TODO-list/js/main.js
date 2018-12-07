d8619efc86f8849bcb2d20becf3728af14364921
hitmax <43930558+hitmax@users.noreply.github.com>
2018-12-07 11:43:58 +0200

    add eventListener keyup ENTER for input[text] homework-js-07 TODO list

--- homework-js-07-TODO-list/js/main.js
+++ homework-js-07-TODO-list/js/main.js
@@ -2,21 +2,30 @@
     $(function () {
         $todoList = $('.todo-list');
 
-        $('button').on('click', function () {
+        function addEvent() {
             $do = $('#input-for-todo').val();
             if ($do) {
-                $('.todo-list')
+                $todoList
                     .children()
                     .append('<li><input type="checkbox"><span>'
                         + $do
                         + '</span><input type="button" class="delete" value="del"></li>');
                 $do = $('#input-for-todo').val('');
             }
+        }
+
+
+        $('.add-button').on('click', addEvent);
+
+        $(window).on('keyup', function (event) {
+            if (event.keyCode === 13) {
+                addEvent();
+            }
         });
 
-        $('html').on('click', '.delete', function () {
+        $todoList.on('click', '.delete', function () {
             $(this).parent().remove();
-        })
+        });
 
         $('#show-completed').on('click', function () {
             $todoList.find('li').hide();
@@ -25,12 +34,12 @@
 
         $('#show-all').on('click', function () {
             $todoList.find('li').show();
-        })
+        });
 
         $('#show-uncompleted').on('click', function () {
             $todoList.find('li').hide();
             $todoList.find('input:checkbox:not(:checked)').parent().show();
-        })
+        });
 
 
     });
