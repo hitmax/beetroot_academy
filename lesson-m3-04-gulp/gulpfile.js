db66eeee7a2002c622a6a9722779392ea054c7dd
hitmax <hitmax.rider@gmail.com>
2019-01-20 14:13:49 +0200

    lesson m3 04 gulp

---
+++ lesson-m3-04-gulp/gulpfile.js
@@ -0,0 +1,107 @@
+//GULP SETTINGS FOR CSS
+
+
+var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer');
+
+
+sass.compiler = require('node-sass');
+
+gulp.task('sassToCSS', function () {
+    return gulp.src('styles/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./styles'));
+});
+
+gulp.task('concatMinifyCssTask', function () {
+    // return gulp.src('styles/**/*.css') //all files in style dir
+    return gulp.src(
+        [
+            'styles/plugin.css',
+            'styles/main.css'
+        ])
+        .pipe(concatCss("styles/bundle.css"))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))                                            //remove from autoprefixerTask for optimization
+        .pipe(minifyCSS({compatibility: 'ie8'}))       //remove from minifyCSSTask for optimization
+        .pipe(gulp.dest('dist/'));
+});
+
+//minifyCSSTask
+/*gulp.task('minifyCSSTask', function () {
+    return gulp.src('./dist/styles/bundle.css')
+        .pipe(minifyCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/styles'));
+});*/
+
+
+//autoprefixerTask
+/*gulp.task('autoprefixerTask', () =>
+    gulp.src('src/app.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
+);*/
+
+
+gulp.task('default', gulp.series('sassToCSS', 'concatMinifyCssTask'));
+
+//GULP SETTINGS FOR JS
+
+var concatJS = require("gulp-concat"),
+    uglify = require('gulp-uglify');
+
+gulp.task('concatMinifyJS', function () {
+    return gulp.src(['./js/jquery-3.3.1.js', './js/main.js'])
+        .pipe(concatJS('bundle.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('default', gulp.series('concatMinifyJS'));
+
+
+//GULP SETTINGS FOR images
+
+var imgOptimize = require('gulp-imagemin');
+// var jpegOptimize = require('imagemin-jpegoptim');              //not yet, mb will learn next time
+
+gulp.task('imgOptimizeTask', function (done) {
+    gulp.src('./images/**/*')
+        .pipe(imgOptimize([
+                imgOptimize.gifsicle({interlaced: true}),
+                imgOptimize.jpegtran({progressive: true}),
+                imgOptimize.optipng({optimizationLevel: 4}),
+                imgOptimize.svgo({
+                    plugins: [
+                        {removeViewBox: true},
+                        {cleanupIDs: false}
+                    ]
+                })
+            ])
+        )
+        .pipe(gulp.dest('dist/images'));
+    done();
+});
+
+gulp.task('default', gulp.series('imgOptimizeTask'));
+
+
+//GULP SETTINGS FOR watchers
+
+//pls add ass homework
+
+
+
+
+
+
+
+
